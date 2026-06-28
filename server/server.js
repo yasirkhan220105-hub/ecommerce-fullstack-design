c2b5c8a76ec9990d733444f415f711576557f775
Yasir Khan <yasirkhan220105@gmail.com>
2026-06-28 22:42:36 +0500

    fix: dotenv config

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+require('dotenv').config({ path: '.env' });
 
 const app = express();
 app.use(cors());
